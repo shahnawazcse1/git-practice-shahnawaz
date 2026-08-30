0bb806141a26cb552e35795598506afed4f4958a
Shahnawazcse1 <shahnawazcse1@gmail.com>
2026-08-30 13:41:47 +0600

    Add basic calculator functions

--- docs/src/main.py
+++
@@ -1,5 +0,0 @@
-from datetime import date
-
-print("Name: Md Shahnawaz")
-
-print("Today's Date:", date.today())
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from datetime import date
+from utils import add, subtract
+
+print("Name: Md Shahnawaz")
+print("Today's Date:", date.today())
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
